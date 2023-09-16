b278fd9da32f07cfb6cbc2daff60e0a4296eee5b
Antara999333 <143966099+Antara999333@users.noreply.github.com>
2023-09-17 02:01:32 +0530

    Update and rename mean_using_pandas.py to statsusingpolars.py

--- statsusingpolars.py
+++ statsusingpolars.py
@@ -1,14 +1,11 @@
-"""
-conducting a basic level statistical analysis using pandas
-"""
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
 # URL of the CSV file
 url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/covid-geography/mmsa-icu-beds.csv"
 
-# Read the CSV data into a Pandas DataFrame
-df = pd.read_csv(url)
+# Read the CSV data into a Polars DataFrame
+df = pl.read_csv(url)
 
 # Calculate summary statistics for the "total_at_risk" column
 total_at_risk_stats = df['total_at_risk'].describe()
@@ -16,17 +13,15 @@ total_at_risk_stats = df['total_at_risk'].describe()
 # Print the summary statistics
 print(total_at_risk_stats)
 
-
 # Create a DataFrame with your summary statistics
-summary_data = pd.DataFrame({
+summary_data = pl.DataFrame({
     'Statistic': ['Mean', 'Median', 'Standard Deviation'],
     'Value': [667188.7, 396081.5, 884786.8]
 })
 
 # Create a bar chart
 plt.figure(figsize=(8, 6))
-plt.bar(summary_data['Statistic'], summary_data['Value'], color=['blue', 'green', 'red'])
+plt.bar(summary_data['Statistic'].to_pandas(), summary_data['Value'].to_pandas(), color=['blue', 'green', 'red'])
 plt.ylabel('Value')
 plt.title('Summary Statistics for "total_at_risk"')
 plt.show()
-
